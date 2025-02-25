db855505ef18c7eeff766e30eea9401a617a3706
Alla Rakhimova <alla.rakhimova103@gmail.com>
2025-02-25 14:35:14 +0200

    add button component

---
+++ src/components/button.tsx
@@ -0,0 +1,9 @@
+function Button() {
+  return (
+    <a className="flex h-36 w-36 cursor-pointer items-center justify-center rounded-full bg-white text-black uppercase">
+      Explore
+    </a>
+  );
+}
+
+export default Button;
