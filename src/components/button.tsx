d15c5d7cc76cccc0b865c3a305e46821015dd8a6
Alla Rakhimova <alla.rakhimova103@gmail.com>
2025-02-25 18:58:34 +0200

    add button animation

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,7 +1,12 @@
+import { TEXT_PRESET_4 } from "../twcssPresets";
+
 function Button() {
   return (
-    <a className="flex h-36 w-36 cursor-pointer items-center justify-center rounded-full bg-white text-black uppercase">
+    <a
+      className={`group relative flex h-36 w-36 cursor-pointer items-center justify-center rounded-full bg-white text-blue-900 uppercase md:h-68 md:w-68 ${TEXT_PRESET_4}`}
+    >
       Explore
+      <span className="absolute inset-0 scale-0 rounded-full bg-white/10 transition-all duration-500 group-hover:scale-200 md:group-hover:scale-150"></span>
     </a>
   );
 }
---
+++ src/twcssPresets.ts
@@ -0,0 +1,18 @@
+import clsx from "clsx";
+
+export const TEXT_PRESET_1 = clsx(
+  "font-bellefair",
+  "text-8xl",
+  "md:text-10xl",
+  "leading-11",
+  "tracking-normal",
+  "uppercase",
+);
+
+export const TEXT_PRESET_4 = clsx(
+  "font-bellefair",
+  "text-lg",
+  "md:text-3xl", // should be 32px
+  "tracking-normal",
+  "uppercase",
+);
